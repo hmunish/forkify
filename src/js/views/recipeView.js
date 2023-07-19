26137d854b09e928d1dc42f4661f79ee727969b8
Munish <hmunish5@gmail.com>
2023-07-19 19:08:26 +0200

    Add npm fractional library to show ingredients in fraction quantity

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from 'url:../../img/icons.svg';
+import { Fraction } from 'fractional';
 class RecipeView {
   #data;
   #parentElement = document.querySelector('.recipe');
@@ -93,7 +94,9 @@ class RecipeView {
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing.quantity}</div>
+            <div class="recipe__quantity">${
+              ing.quantity ? new Fraction(ing.quantity) : ''
+            }</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ing.unit}</span>
               ${ing.description}

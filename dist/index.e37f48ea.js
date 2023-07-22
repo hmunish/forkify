b5aa0c85677bdbba7d6a9bd66bd00e94feeae3d2
Munish <hmunish5@gmail.com>
2023-07-22 19:15:30 +0200

    Fix bookmarks user generated icon bug

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -3457,6 +3457,11 @@ class BookmarksView extends (0, _viewDefault.default) {
           <div class="preview__data">
             <h4 class="preview__title">${result.title}</h4>
             <p class="preview__publisher">${result.publisher}</p>
+            <div class="preview__user-generated ${result.key ? "" : "hidden"}">
+            <svg>
+              <use href="${0, _iconsSvgDefault.default}#icon-user"></use>
+            </svg>
+          </div>
           </div>
         </a>
       </li>
--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -27,6 +27,11 @@ class BookmarksView extends View {
           <div class="preview__data">
             <h4 class="preview__title">${result.title}</h4>
             <p class="preview__publisher">${result.publisher}</p>
+            <div class="preview__user-generated ${result.key ? '' : 'hidden'}">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
+          </div>
           </div>
         </a>
       </li>
